78d8e503faec6f26399b6f1323106885ad0dc0d1
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-02 00:08:35 -0300

    Exercise done

--- src/CodeWars/ReplaceLetterWithAlphabetPosition.js
+++ src/CodeWars/ReplaceLetterWithAlphabetPosition.js
@@ -1,3 +1,12 @@
-//Alphabet arrays:
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const alphabetUp = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+const value = prompt('Type any string').toLowerCase().trim();
+
+let valPos = value.split('').map((letter) => {
+  if (alphabet.includes(letter)) {
+    return alphabet.indexOf(letter) + 1;
+  } //else:
+  return ' ';
+});
+
+valPos = valPos.join().replace(',,', '');
+console.log(`${value} == ${valPos}`);
