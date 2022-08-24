b1c47f72e6c8520d598e27d2aac4faa59c0c4ecb
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-24 00:39:06 -0300

    Exercise done ✔

---
+++ src/LeetCode/FizzBuzz.ts
@@ -0,0 +1,22 @@
+function FizzBuzz(times: number): string[] {
+  let result: string[] = [];
+
+  for (let i = 1; i <= times; i++) {
+    let output: string = '';
+    if (i % 3 === 0 && i % 5 === 0) {
+      output += 'FizzBuzz';
+    } else if (i % 3 === 0) {
+      output += 'Fizz';
+    } else if (i % 5 === 0) {
+      output += 'Buzz';
+    } else {
+      output += i;
+    }
+    result = [...result, output];
+  }
+
+  return result;
+}
+
+console.log(FizzBuzz(15));
+// Run this function ☝🏽 times
