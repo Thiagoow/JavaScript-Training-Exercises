399dd10db62df98f99e4c11442d69cb7d103cb5b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-24 00:05:31 -0300

    Exercise done ✔

---
+++ src/LeetCode/SumArray.ts
@@ -0,0 +1,22 @@
+const nums: number[] = [1, 1, 1, 1];
+
+function runningSum(nums: number[]): number[] {
+  let result: number[] = new Array(nums.length);
+  result[0] = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] + nums[i];
+  }
+  return result;
+}
+
+console.log(runningSum(nums));
+
+/* Doing this with map & ternary
+operator instead of for loop:
+
+nums.map((num, index) => {
+  index > 0
+    ? (result[index] = result[index - 1] + num)
+    : (result[index] = num);
+}); */
