aa7d92f9f3de8914f59b5082159460a235e393de
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-19 17:08:59 -0300

    Functions order on code switched

--- src/Random/AlphabetLetter.js
+++ src/Random/AlphabetLetter.js
@@ -7,32 +7,6 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const alphabetUp = alphabet.map((letter) => letter.toUpperCase());
 console.log(alphabetUp);
 
-function distanceBetweenTwoLetters() {
-  const isValid1 = alphabet.includes(letter1);
-  const isValid2 = alphabet.includes(letter2);
-
-  let valid = false;
-  if (isValid1 && isValid2) {
-    valid = true;
-  }
-
-  if (valid) {
-    //+1 pq array começa em 0:
-    const indexOfFirst = alphabet.indexOf(letter1) + 1;
-    const indexOfLast = alphabet.indexOf(letter2) + 1;
-
-    //-1 pq é quantas letras existem entre essas letras:
-    const res = indexOfLast - indexOfFirst - 1;
-
-    console.log(
-      `A distância entre "${letter1.toUpperCase()}" e "${letter2.toUpperCase()}" é de ${res} letras`
-    );
-  } else {
-    console.log('Ambos ou 1 dos valores recebidos não era uma letra válida❗');
-  }
-}
-distanceBetweenTwoLetters();
-
 function distanceToLastLetter(letra1) {
   const isString = alphabet.includes(letra1);
 
@@ -60,4 +34,30 @@ function distanceToLastLetter(letra1) {
     console.log('Q? 🤔');
   }
 }
-distanceToLastLetter('a');
+distanceToLastLetter(letter1);
+
+function distanceBetweenTwoLetters() {
+  const isValid1 = alphabet.includes(letter1);
+  const isValid2 = alphabet.includes(letter2);
+
+  let valid = false;
+  if (isValid1 && isValid2) {
+    valid = true;
+  }
+
+  if (valid) {
+    //+1 pq array começa em 0:
+    const indexOfFirst = alphabet.indexOf(letter1) + 1;
+    const indexOfLast = alphabet.indexOf(letter2) + 1;
+
+    //-1 pq é quantas letras existem entre essas letras:
+    const res = indexOfLast - indexOfFirst - 1;
+
+    console.log(
+      `A distância entre "${letter1.toUpperCase()}" e "${letter2.toUpperCase()}" é de ${res} letras`
+    );
+  } else {
+    console.log('Ambos ou 1 dos valores recebidos não era uma letra válida❗');
+  }
+}
+distanceBetweenTwoLetters();
