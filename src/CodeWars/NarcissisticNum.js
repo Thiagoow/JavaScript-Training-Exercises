6dfca92faaf02c932086ce73aa686e46cd90ccdd
Thiago Lopes <thiagoslopes8@outlook.com>
2025-12-13 18:57:18 -0300

    refact: old useless code

--- src/CodeWars/NarcissisticNum.js
+++ src/CodeWars/NarcissisticNum.js
@@ -1,23 +1,18 @@
 const value = prompt('Type an integer number');
 let isNarcissist = false;
-
-//Convert to string to check it's length:
-let valStr = value.toString();
-//Split value to get all the digits && put on a Array:
-let valArr = valStr.split('');
-//console.log(valStr, valArr);
+let valArr = value.toString().split('');
 
 let digitPow,
   sum = 0;
 
 for (let i = 0; i < valArr.length; i++) {
   digitPow = Math.pow(valArr[i], valArr.length);
-  //console.log(digitPow);
   sum += digitPow;
+  console.log(digitPow);
 }
 console.log(`The sum is: ${sum}`);
 
-sum === value ? (isNarcissist = true) : (isNarcissist = false);
-isNarcissist
-  ? console.log(`✔ ${value} is a Narcissistic Number`)
-  : console.log(`❌ ${value} ISN'T a Narcissistic Number`);
+sum == value ? (isNarcissist = true) : (isNarcissist = false);
+console.log(
+  `\n${value} ${isNarcissist ? '✅ is' : '❌ is not'} a Narcissistic Number`
+);
--- src/CodeWars/ReplaceLetterWithAlphabetPosition.js
+++ src/CodeWars/ReplaceLetterWithAlphabetPosition.js
@@ -1,12 +1,11 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const value = prompt('Type any string').toLowerCase().trim();
 
-let valPos = value.split('').map((letter) => {
+const valPos = value.split('').map((letter) => {
   if (alphabet.includes(letter)) {
     return alphabet.indexOf(letter) + 1;
   } //else:
   return ' ';
 });
 
-valPos = valPos.join().replace(',,', '');
 console.log(`${value} == ${valPos}`);
